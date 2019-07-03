5abeef387613f67e2aa131504146f8a727470750
moon <moonheeyeon@likelion.org>
2019-07-03 18:30:30 +0900

    “test”
    Signed-off-by: moon <moonheeyeon@likelion.org>

---
+++ crud/burok/c/crud/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path, include
+import viewcrud.urls
+import viewcrud.views
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', viewcrud.views.welcome, name="welcome"),
+    path('funccrud/', include(viewcrud.urls)),
+    path('classcrud/', include(viewcrud.urls)),
+]
